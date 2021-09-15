1bbe475bf0baf7d98578a41b9a15dbaa57a781c5
Robbert Broersma <mail@robbertbroersma.nl>
2021-09-15 23:50:29 +0200

    docs: color design token search

--- documentation/components/util.js
+++ documentation/components/util.js
@@ -1,3 +1,8 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Robbert Broersma
+ */
+
 export const cssVariable = (token) => `var(--${token.path.join('-')})`;
 
 export const styleDictionaryRef = (token) => `{${token.path.join('.')}.value}`;
