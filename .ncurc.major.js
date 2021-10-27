4ada6acb235789492adb1eb4fa4d24168d3021b4
Robbert Broersma <mail@robbertbroersma.nl>
2021-10-27 13:25:16 +0200

    build: install npm-check-updates

---
+++ .ncurc.major.js
@@ -0,0 +1,24 @@
+const minorConfig = require('./.ncurc.minor');
+
+module.exports = {
+  reject: [
+    ...minorConfig.reject,
+    '@angular/cli',
+    '@angular/compiler',
+    '@angular/compiler-cli',
+    '@angular/core',
+    '@angular/forms',
+    '@percy/storybook',
+    '@types/react',
+    '@types/react-dom',
+    'css-loader',
+    'color',
+    'ng-packagr',
+    'react',
+    'react-dom',
+    'rxjs',
+    'sass-loader',
+    'style-loader',
+    'webpack',
+  ],
+};
